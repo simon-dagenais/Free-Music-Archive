7224146172475bdec38f1cc9bc569b4da31b98cd
Simon Dagenais <simon.dagenais@voltacharging.com>
2022-08-24 08:21:29 -0400

    function to extract data

---
+++ extract_data.py
@@ -0,0 +1,86 @@
+import json
+import os
+import math
+import librosa
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30  # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+
+
+def process_metadata():
+    tracks = pd.read_csv("fma_metadata/tracks.csv", skiprows=1)
+    tracks["track"] = tracks["Unnamed: 0"].apply(lambda x: str(x).zfill(6))
+    tracks["subdir"] = tracks["Unnamed: 0"].apply(lambda x: str(x)[:-3].zfill(3))
+    tracks["path"] = "fma_small" + "/" + tracks["subdir"] + "/" + tracks["track"] + ".mp3"
+    tracks = tracks.iloc[1:, :]
+    lb_make = LabelEncoder()
+    tracks["genre_label"] = lb_make.fit_transform(tracks["genre_top"])
+
+    return tracks
+
+
+def save_mfcc(num_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+    """Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
+        :param dataset_path (str): Path to dataset
+        :param json_path (str): Path to json file used to save MFCCs
+        :param num_mfcc (int): Number of coefficients to extract
+        :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
+        :param hop_length (int): Sliding window for FFT. Measured in # of samples
+        :param: num_segments (int): Number of segments we want to divide sample tracks into
+        :return:
+        """
+
+    tracks = process_metadata()
+
+    # dictionary to store mapping, labels, and MFCCs
+    data = {
+        "mapping": [],
+        "labels": [],
+        "mfcc": []
+    }
+
+    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+
+    for split in tracks["split"].unique():
+
+        df_i = tracks[tracks["split"] == split]
+
+        for i, row in df_i.iloc[0: 500, :].iterrows():
+            # semantic_label = row["genre_top"]
+            # data["mapping"].append(semantic_label)
+
+            try:
+                signal, sample_rate = librosa.load(row["path"], sr=SAMPLE_RATE)
+
+                # process all segments of audio file
+                for d in range(num_segments):
+
+                    # calculate start and finish sample for current segment
+                    start = samples_per_segment * d
+                    finish = start + samples_per_segment
+
+                    # extract mfcc, transpose
+                    mfcc = librosa.feature.mfcc(y=signal[start:finish], sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
+                                                hop_length=hop_length).T
+
+                    # store only mfcc feature with expected number of vectors
+                    if len(mfcc) == num_mfcc_vectors_per_segment:
+                        data["mfcc"].append(mfcc.tolist())
+                        data["labels"].append(row["genre_label"])
+                        print("{}, segment:{}".format(row["path"], d + 1))
+            except FileNotFoundError:
+                continue
+
+        json_path = f"{split}_data_fma_small.json"
+
+        # save MFCCs to json file
+        with open(json_path, "w") as fp:
+            json.dump(data, fp, indent=4)
+
+
+if __name__ == "__main__":
+    save_mfcc(num_segments=10)
